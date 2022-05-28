bdc48cfa0485f1663c131c0e52c401c7abb288bf
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-05-28 16:50:58 -0300

    att

--- time/src/App.js
+++ time/src/App.js
@@ -1,23 +1,52 @@
 import React, { Component} from 'react';
+import "./styles/reset.css";
+import "./styles/app.css";
+import github from "./imgs/github.svg"
+import Instagram from "./imgs/Instagram.svg"
+import Linkedin from "./imgs/Linkedin.svg"
 
 class App extends Component {
     constructor() {
-        super();
+        super(); //poder pegar variaveis global
         this.state = {
             hora: "00:00:00"
+            //defino a variavel
         }
     }
 
     componentDidMount(){
         setInterval(()=>{
             this.setState({hora: new Date().toLocaleTimeString()})
-        },1000); //1 segundo
+            //captura a hora atual e atualiza a variavel
+        },1000); //1 segundo (intervalo)
     }
 
     render() {
         return(
-            <div>
-                <h1>hora {this.state.hora}</h1>
+            <div id="pagina">
+                <div className="container-titulo">
+                    <h1 id="titulo">Time</h1>
+                </div>
+                <div className="container-hora">
+                    <h2 id="hora">{this.state.hora}</h2>
+                </div>
+                <div className="container-links">
+                    <div class="links-1">
+                        <a href="https://github.com/fullzer4" className="card-links">
+                            <img src={github} alt="github"/>
+                        </a>
+                    </div>
+                    <div class="links-2">
+                        <a href="https://github.com/fullzer4" className="card-links">
+                            <img src={Instagram} alt="instagram"/>
+                        </a>
+                    </div>
+                    <div class="links-3">
+                        <a href="https://github.com/fullzer4" className="card-links">
+                            <img src={Linkedin} alt="linkedin"/>
+                        </a>
+                    </div>
+                </div>
             </div>
         )
     }
